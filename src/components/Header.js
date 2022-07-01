6ccb14c6e95d66b884b2f334ddf58924f1f78de0
jalagor <johnrossrogla@gmail.com>
2022-06-30 19:59:55 -0600

    added header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 export default function Header() {
   return (
-    <div classname = 'header' >
+    <div className = 'header' >
+
+        <p>Home</p>
         <p>About</p>
         <p>Contact</p>
         <p>Projects</p>
